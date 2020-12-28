21ef22eed76b1a2edf9b00b97ee90b9560f863f8
smallville7123 <smallville7123@gmail.com>
2020-12-28 22:14:31 +1000

    scroll view does not automatically expand

--- clipview/src/main/java/smallville7123/example/clipview/ClipView.java
+++ clipview/src/main/java/smallville7123/example/clipview/ClipView.java
@@ -7,8 +7,12 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 
 import java.util.ArrayList;
 
@@ -31,10 +35,16 @@ public class ClipView extends HorizontalScrollView {
     }
 
     Context mContext;
+    AttributeSet mAttrs;
     FrameLayout content;
 
     private void init(Context context, AttributeSet attrs) {
         mContext = context;
+        mAttrs = attrs;
+
+        // make scroll view match parent width
+        setFillViewport(true);
+
         FrameLayout frame = new FrameLayout(context, attrs);
         content = frame;
         frame.setLayoutParams(
@@ -45,30 +55,136 @@ public class ClipView extends HorizontalScrollView {
         );
         frame.setTag(Internal);
         addView(frame);
-
-        FrameLayout A = new FrameLayout(context, attrs);
-        A.setBackgroundColor(Color.GREEN);
-        A.setLayoutParams(
-                new ViewGroup.MarginLayoutParams(
-                        1000,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                )
-        );
-        FrameLayout B = new FrameLayout(context, attrs);
-        B.setBackgroundColor(Color.RED);
-        B.setLayoutParams(
-                new ViewGroup.MarginLayoutParams(
-                        2200,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                )
-        );
-        B.setX(1200);
-        addView(A);
-        addView(B);
+        Clip A = newClip();
+        A.setColor(Color.GREEN);
+        A.setX(800);
+        A.setWidth(1000);
+        addClip(A);
     }
+
+    class Clip {
+        View content;
+
+        Clip(Context context) {
+            content = new FrameLayout(context);
+            setWidth(100);
+        }
+
+        Clip(Context context, AttributeSet attrs) {
+            content = new FrameLayout(context, attrs);
+            setWidth(100);
+        }
+
+        public Clip(View content) {
+            content = content;
+        }
+
+        public void setColor(@ColorInt int color) {
+            content.setBackgroundColor(color);
+        }
+
+        public void setX(int x) {
+            content.setX(x);
+        }
+
+        public float getX() {
+            return content.getX();
+        }
+
+        public void setWidth(int width) {
+            content.setLayoutParams(
+                    new ViewGroup.LayoutParams(
+                            width,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+            );
+        }
+
+        public int getWidth() {
+            return content.getWidth();
+        }
+
+        public ViewPropertyAnimator animate() {
+            return content.animate();
+        }
+    }
+
+    Clip newClip() {
+        return new Clip(mContext, mAttrs);
+    };
 
     private static class Internal {}
     Internal Internal = new Internal();
+    ArrayList<Clip> clips = new ArrayList<>();
+
+    public void addClip(Clip clip) {
+        clips.add(clip);
+        content.addView(clip.content);
+    }
+
+    private static final String TAG = "ClipView";
+
+    private float relativeToViewX;
+    private float relativeToViewY;
+    private float relativeToViewScrollX;
+    private float relativeToViewScrollY;
+
+    boolean scrolling = false;
+    boolean clipTouch = false;
+    Clip touchedClip;
+    float originalX;
+    float downDX;
+    float downRawX;
+    float currentRawX;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent() called with: event = [" + MotionEvent.actionToString(event.getAction()) + "]");
+        currentRawX = event.getRawX();
+        relativeToViewX = event.getX();
+        relativeToViewScrollX = relativeToViewX + getScrollX();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                for (Clip clip : clips) {
+                    float clipStart = clip.getX();
+                    float clipEnd = clipStart + clip.getWidth();
+                    Log.d(TAG, "clipStart = [ " + clipStart + "]");
+                    Log.d(TAG, "clipEnd = [ " + clipEnd + "]");
+                    Log.d(TAG, "relativeToViewScrollX = [ " + relativeToViewScrollX + "]");
+                    if (relativeToViewScrollX >= clipStart && relativeToViewScrollX <= clipEnd) {
+                        clipTouch = true;
+                        touchedClip = clip;
+                        originalX = clipStart;
+                        downRawX = currentRawX;
+                        downDX = originalX - downRawX;
+                        break;
+                    }
+                }
+                if (!clipTouch) scrolling = true;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                if (clipTouch) clipTouch = false;
+                else scrolling = false;
+                break;
+        }
+        return clipTouch ? onClipTouchEvent(touchedClip, event) : super.onTouchEvent(event);
+    }
+
+    public boolean onClipTouchEvent(Clip clip, MotionEvent event) {
+        Log.d(TAG, "onClipTouchEvent() called with: clip = [" + clip + "], event = [" + MotionEvent.actionToString(event.getAction()) + "]");
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            clip.animate().x(currentRawX + downDX).setDuration(0).start();
+        }
+        return true;
+    }
+
+
+
+
+
+
+
 
     /**
      * <p>Adds a child view. If no layout parameters are already set on the child, the
@@ -163,71 +279,6 @@ public class ClipView extends HorizontalScrollView {
     public void addView(View child, int index, ViewGroup.LayoutParams params) {
         Object tag = child.getTag();
         if (tag instanceof Internal) super.addView(child, index, params);
-        else addClip(child);
-    }
-
-    ArrayList<View> clips = new ArrayList<>();
-
-    public void addClip(View view) {
-        clips.add(view);
-        content.addView(view);
-    }
-
-    private static final String TAG = "ClipView";
-
-    private float relativeToViewX;
-    private float relativeToViewY;
-    private float relativeToViewScrollX;
-    private float relativeToViewScrollY;
-
-    boolean scrolling = false;
-    boolean clipTouch = false;
-    View touchedClip;
-    float originalX;
-    float downDX;
-    float downRawX;
-    float currentRawX;
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        Log.d(TAG, "onTouchEvent() called with: event = [" + MotionEvent.actionToString(event.getAction()) + "]");
-        currentRawX = event.getRawX();
-        relativeToViewX = event.getX();
-        relativeToViewScrollX = relativeToViewX + getScrollX();
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                for (View clip : clips) {
-                    MarginLayoutParams params = (MarginLayoutParams) clip.getLayoutParams();
-                    float clipStart = clip.getX();
-                    float clipEnd = clipStart + clip.getWidth();
-                    Log.d(TAG, "clipStart = [ " + clipStart + "]");
-                    Log.d(TAG, "clipEnd = [ " + clipEnd + "]");
-                    Log.d(TAG, "relativeToViewScrollX = [ " + relativeToViewScrollX + "]");
-                    if (relativeToViewScrollX >= clipStart && relativeToViewScrollX <= clipEnd) {
-                        clipTouch = true;
-                        touchedClip = clip;
-                        originalX = clipStart;
-                        downRawX = currentRawX;
-                        downDX = originalX - downRawX;
-                        break;
-                    }
-                }
-                if (!clipTouch) scrolling = true;
-                break;
-            case MotionEvent.ACTION_CANCEL:
-            case MotionEvent.ACTION_UP:
-                if (clipTouch) clipTouch = false;
-                else scrolling = false;
-                break;
-        }
-        return clipTouch ? onClipTouchEvent(touchedClip, event) : super.onTouchEvent(event);
-    }
-
-    public boolean onClipTouchEvent(View clip, MotionEvent event) {
-        Log.d(TAG, "onClipTouchEvent() called with: clip = [" + clip + "], event = [" + MotionEvent.actionToString(event.getAction()) + "]");
-        if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            clip.animate().x(currentRawX + downDX).setDuration(0).start();
-        }
-        return true;
+        else addClip(new Clip(child));
     }
 }
